af416af377ff6033d32888d2d855c0d58e1ab793
atomsk-0 <wexberkyhd@gmail.com>
2024-11-02 12:32:01 +0200

    initial commit

---
+++ src/Moonlit/Manager/ProcessManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+
+namespace Moonlit.Manager;
+
+public static unsafe class ProcessManager
+{
+    /// <summary>
+    /// Currently opened process
+    /// </summary>
+    public static Process? CurrentProcess;
+
+    /// <summary>
+    /// Open process by name
+    /// </summary>
+    /// <param name="name">name of process</param>
+    /// <returns>true if successful</returns>
+    public static bool Open(string name)
+    {
+        CurrentProcess?.Dispose();
+        var processes = Process.GetProcessesByName(name);
+        if (processes.Length == 0) return false;
+        CurrentProcess = processes[0];
+        return true;
+    }
+
+    public static bool Open(int id)
+    {
+        CurrentProcess?.Dispose();
+        try
+        {
+            CurrentProcess = Process.GetProcessById(id);
+            return true;
+        }
+        catch (ArgumentException)
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Get all processes
+    /// </summary>
+    /// <returns>Array of processes</returns>
+    public static Process[] GetProcesses()
+    {
+        return Process.GetProcesses();
+    }
+
+    /// <summary>
+    /// Get all processes with main window handle
+    /// </summary>
+    /// <returns>Array of processes</returns>
+    public static Process[] GetWindows()
+    {
+        return Process.GetProcesses().Where(p => p.MainWindowHandle != IntPtr.Zero).ToArray();
+    }
+
+    /// <summary>
+    /// Get all application processes
+    /// </summary>
+    /// <returns>Array of processes</returns>
+    public static Process[] GetApplications()
+    {
+        return Process.GetProcesses().Where(p => !string.IsNullOrEmpty(p.MainWindowTitle)).ToArray();
+    }
+}
---
+++ src/Moonlit/Memory.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Moonlit;
+
+public static unsafe class Memory
+{
+    public static byte* BaseAddress;
+    public static byte* EndAddress;
+
+    public static byte* StringToBytes(string str, uint* length)
+    {
+        // Remove spaces
+        str = str.Replace(" ", "");
+        *length = (uint)str.Length / 2;
+        byte* bytes = (byte*)NativeMemory.Alloc(*length); // Heap allocation, remember to free it after use
+        for (uint i = 0; i < *length; i++)
+        {
+            string hexPair = str.Substring((int)(i * 2), 2);
+            bytes[i] = byte.Parse(hexPair, System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        return bytes;
+    }
+}
